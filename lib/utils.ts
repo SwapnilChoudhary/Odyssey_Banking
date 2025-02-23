0c1629b0f8b6d2c7e4d2590fbc15b7ff67df35f5
SwapnilChoudhary <swapnilchoudhary1429@gmail.com>
2025-02-24 00:05:02 +0530

    Implement auth, appwrite, and sentry.

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,18 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+  // sign up
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(1).max(25),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(25),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(20),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  adhaarNumber: type === 'sign-in' ? z.string().optional() : z.string().min(12).max(12),
+  // both
+  email: z.string().email().min(8).max(35),
+  password: z.string().min(8).max(35)
+})
